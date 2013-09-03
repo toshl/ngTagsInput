8f5f77daa8b4c8d24ce604378827722932f8364c
Domen <domen.zajc@gmail.com>
2013-09-03 13:37:19 +0200

    Modify template (remove button).

--- src/ng-tags-input.js
+++ src/ng-tags-input.js
@@ -13,7 +13,7 @@ angular.module('tags-input', []).directive('tagsInput', function() {
         template: '<div class="ngTagsInput {{ cssClass }}">' +
                   '  <input type="text" placeholder="{{ placeholder }}" size="{{ placeholder.length }}" maxlength="{{ maxLength }}" tabindex="{{ tabindex }}" ng-model="newTag">' +
                   '  <ul>' +
-                  '    <li ng-repeat="tag in tags" ng-class="getCssClass($index)"><span>{{ tag }}</span><button type="button" ng-click="remove($index)">{{ removeTagSymbol }}</button></li>' +
+                  '    <li ng-repeat="tag in tags" ng-class="getCssClass($index)" ng-click="remove($index)"><span>{{ tag }}</span></li>' +
                   '  </ul>' +
                   '</div>',
         controller: ['$scope', '$attrs', function($scope, $attrs) {
